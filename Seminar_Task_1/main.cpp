463ca3b2a82c5105e31feb46577b5435e383c197
Kirill <kirill_131097@mail.ru>
2019-09-29 17:21:19 +0300

    Seminar_Task_1: Select tasks from current path

--- Seminar_Task_1/main.cpp
+++ Seminar_Task_1/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "CuttingTask.h"
 int main() {
+    std::string fsPath = std::filesystem::current_path().parent_path().string();
     CCuttingTask<int> Task(eALGORITHM_GREEDY, eSTRATEGY_CUSTOM_SORTING);
-    Task.SetTasksPath("/home/trojan52/UNN/Seminar-Tasks/Seminar_Task_1/Tests/task_1_03_n100.txt");
+    Task.SetTasksPath(fsPath + "/Tests/task_1_03_n100.txt");
     Task.Solve();
     Task.PrintData();
     return 0;
